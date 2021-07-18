ba2f45135263635bd3a8daa76a308922af28acaa
Tarun Gavara <tarungavara@gmail.com>
2021-07-18 23:27:02 +0530

    add restic restore script

---
+++ scripts/05-restic.sh
@@ -0,0 +1,17 @@
+# run this script as zsh 05-restic.sh
+
+# install restic
+sudo apt install -y restic
+
+# set env variables
+echo "export RESTIC_REPOSITORY='/mnt/ext_drive/luna_backup'" >> ~/.zshrc
+source ~/.zshrc
+
+## initialize a restic repo on /mnt/ext_drive
+#restic init --repo $RESTIC_REPOSITORY
+#
+## backup ~/luna_data to /mnt/ext_drive/luna_backup
+#cd ~ && sudo -E restic --verbose backup luna_data && cd -
+
+# restore luna_data from backup on /mnt/ext_drive
+sudo -E restic --verbose restore latest --target ~/
