81671210d95d5f51cf7e41c7cabec61a4b79b70c
Tarun Gavara <tarungavara@gmail.com>
2021-07-18 15:38:15 +0530

    change

--- scripts/01-setup.sh
+++ scripts/01-setup.sh
@@ -10,6 +10,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's+ZSH_THEME="robbyrussell"+ZSH_THEME="powerlevel10k/powerlevel10k"+g' ~/.zshrc
 
+echo 'export PATH=$PATH:/usr/sbin' >> ~/.zshrc
+
 # install docker
 curl -fsSL https://get.docker.com | sh
 
